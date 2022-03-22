3ace6c7242471febaefaacac9d38493ce0b73d5e
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-03-22 22:32:33 +0000

    code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Samuel Carlson All rights reserved
+//
+// Created by: Samuel Carlson
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = '5 + 5 + 3 + 3 = ' + (5 + 5 + 3 +3)
+  document.getElementById('multiply-math').innerHTML = '5 * 3 =  ' + (5 * 3)
+}
+
